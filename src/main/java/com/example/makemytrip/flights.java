bfd3ef11f0e56d32cfc6b2ec82b97a43355b08bb
Abhay <abhaymahalley123@gmail.com>
2023-11-03 13:29:58 +0530

    codeRefactor

---
+++ src/main/java/com/example/makemytrip/flights.java
@@ -0,0 +1,13 @@
+package com.example.makemytrip;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class flights{
+    @GetMapping("/flights")
+    public String getData() {return "book your flights from nagpur to pune with 10% of discount" ;}
+}
+
+
+
---
+++ src/main/java/com/example/makemytrip/hotel.java
@@ -0,0 +1,10 @@
+package com.example.makemytrip;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class hotel{
+    @GetMapping("/hotel")
+    public String getData() {return "book your lavish hotel in the state of maharashtra specially in pune nagpur" ;}
+}
